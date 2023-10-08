925dccc07e179b0cb9b52dc9bfaffa6178dda85b
shikha Mishra <shikha99569@gmail.com>
2023-10-08 18:33:15 +0530

    attempt1

--- src/main/java/com/driver/controllers/PaymentController.java
+++ src/main/java/com/driver/controllers/PaymentController.java
@@ -1,7 +1,10 @@
 package com.driver.controllers;
 
+import com.driver.model.Payment;
 import com.driver.services.impl.PaymentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -18,6 +21,14 @@ public class PaymentController {
         //If the amountSent is less than bill, throw "Insufficient Amount" exception, otherwise update payment attributes
         //If the mode contains a string other than "cash", "card", or "upi" (any character in uppercase or lowercase), throw "Payment mode not detected" exception.
         //Note that the reservationId always exists
-        return null
+        Payment payment;
+        try{
+            payment = paymentService.pay(reservationId, amountSent, mode);
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.getMessage());
+        }
+        return payment;
     }
 }
--- src/main/java/com/driver/services/ParkingLotService.java
+++ src/main/java/com/driver/services/ParkingLotService.java
@@ -1,5 +1,8 @@
 package com.driver.services;
 
+import com.driver.model.Spot;
+import com.driver.model.ParkingLot;
+
 public interface ParkingLotService {
     
     void deleteSpot(int spotId);
